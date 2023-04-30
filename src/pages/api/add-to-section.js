3c6eaf19861412d682db3abbc08f69a7af540e03
maxtan84 <mthockey84@gmail.com>
2023-04-29 22:20:46 -0400

    updated code

--- src/pages/api/add-to-section.js
+++ src/pages/api/add-to-section.js
@@ -1,5 +1,5 @@
 // add-to-section.js
-import { collection, addDoc, doc, updateDoc, arrayUnion } from "firebase/firestore"; 
+import { collection, addDoc, doc, updateDoc, arrayUnion, getDoc } from "firebase/firestore"; 
 import {db, auth} from "../../../firebase";
 import EventEmitter from 'events';
 const workflow = new EventEmitter();
@@ -7,12 +7,39 @@ const workflow = new EventEmitter();
 export default function add_to_section(req, res) {
     const section = req.body.sectionId
     const student = req.body.studentId
-    const studentRef = doc(db, "students", student);
 
-    workflow.once("addSection", () => {
-        updateDoc(studentRef, { sections: arrayUnion(section) })
-        .then(studentDoc => { 
-            workflow.emit("success", studentDoc) 
+    workflow.once("checkParams", () => {
+        if (
+            student
+            && section
+        )
+        {
+            workflow.emit("findUser")
+        }
+        else {
+            workflow.emit("error", "bad params")
+        }
+    })
+
+    workflow.once("findUser", () => {
+        getDoc(doc(db, "students", student))
+        .then(studentRef => {
+            if (studentRef.exists()){
+                workflow.emit("addToSection", studentRef.id)
+            }
+            else {
+                workflow.emit("error", "Id not found")
+            }             
+        })  
+        .catch(err => {
+            workflow.emit("error", err.message)
+        })
+    })
+    
+    workflow.once("addToSection", (studentId) => {
+        updateDoc(doc(db, "students", studentId), { sections: arrayUnion(section) })
+        .then(studentId => { 
+            workflow.emit("success", studentId) 
         })  
         .catch(err => {
             workflow.emit("error", err.message)
@@ -27,5 +54,5 @@ export default function add_to_section(req, res) {
         return res.status(200).json({message})
     })
 
-    workflow.emit("addSection")
+    workflow.emit("checkParams")
 }
