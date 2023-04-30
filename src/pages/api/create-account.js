dbdf7c657d4998f255335734e7a358451819c9fa
maxtan84 <mthockey84@gmail.com>
2023-04-29 22:09:02 -0400

    updated

--- src/pages/api/create-account.js
+++ src/pages/api/create-account.js
@@ -1,4 +1,4 @@
-//create-account.js
+// create-account.js
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore"; 
 import {db, auth} from "../../../firebase";
@@ -8,6 +8,13 @@ const workflow = new EventEmitter();
 export default function create_account(req, res) {
     const email = req.body.email
     const password = req.body.password
+
+    workflow.once("checkParams", () => {
+        if ( email && password ) {
+            workflow.emit("createEmailWithPassword")
+        }
+        else { workflow.emit("error") }
+    })
 
     workflow.once("createEmailWithPassword", () => {
         createUserWithEmailAndPassword(auth, email, password)
@@ -21,21 +28,31 @@ export default function create_account(req, res) {
 
     workflow.once("createDbUser", (userId) => {
         addDoc(collection(db, "students"), { email: email, sections: [], skills: [] })
-        .then(student => {
-            workflow.emit("success", student.id, userId)
+        .then(user => {
+            workflow.emit("createToken", userId)
         })  
         .catch(err => {
             workflow.emit("error", err.message)
         })
     })
 
-    workflow.once("success", (studentId, userId) => {
-        return res.status(200).json({ studentId, userId}) //isn't studentId the same ID as userID?
+    workflow.once("createToken", (userId) => {
+        addDoc(collection(db, "tokens"), { userId, expiryTime: Math.floor(+new Date / 1000) + 60*60*2 })
+        .then(token => {
+            workflow.emit("success", userId, token.id)
+        })  
+        .catch(err => {
+            workflow.emit("error", err.message)
+        })
+    })
+
+    workflow.once("success", (userId, tokenId) => {
+        return res.status(200).json({ userId, tokenId }) 
     })
 
     workflow.once("error", (message) => {
         return res.status(200).json({message})
     })
 
-    workflow.emit("createEmailWithPassword")
+    workflow.emit("checkParams")
 }
