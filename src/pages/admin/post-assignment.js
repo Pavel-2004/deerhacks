bb8bbf8e07c05d81914ff7042d245a8a707e4d54
Ted <97199415+teddrako@users.noreply.github.com>
2023-04-30 04:22:39 -0400

    Style Changes Post Assignment

--- src/pages/admin/post-assignment.js
+++ src/pages/admin/post-assignment.js
@@ -49,12 +49,12 @@ export default function PostAssignment() {
   }
 
   return (
-    <div className={"w-full flex-col"}>
-      <div className={"flex flex-row"}>
-        <p>Assignment Creator</p>
+    <div className={"w-full h-full flex-col bg-[#B5FFE1]"}>
+      <div className={"flex flex-row ml-1"}>
+        <p className={"text-center my-6 font-bold text-3xl text-[#00241B]"} >Assignment Creator</p>
       </div>
-      <div className={"flex flex-row"}>
-        <p>Select Section</p>
+      <div className={"flex flex-row ml-1"}>
+        <p className={"text-center mr-3 font-bold ml-3 text-[#00241B]"} >Select Section</p>
         <select value={sectionId} onChange={e => setSectionId(e.target.value)}>
           <option>Select</option>
           {
@@ -69,16 +69,16 @@ export default function PostAssignment() {
       {
         tasks.length === 0 && (
           <>
-            <div className={"flex flex-col"}>
-              <p>Assignment Name:</p>
-              <input value={assignmentName} onChange={e => setAssignmentName(e.target.value)} placeholder={"Enter name of assignment"} />
+            <div className={"flex flex-col my-4 "}>
+              <p className={"flex flex-row ml-4 font-bold mb-1 text-xl text-[#00241B]"} >Assignment Name:</p>
+              <input className={"pl-4"} value={assignmentName} onChange={e => setAssignmentName(e.target.value)} placeholder={"Enter name of assignment"} />
             </div>
             <div className={"flex flex-col"}>
-              <p>Assignment Description:</p>
-              <textarea value={descripiton} onChange={e => setDescription(e.target.value)} placeholder={"Enter description of assignment"} />
+              <p className={"flex flex-row ml-4 font-bold mb-1 text-xl text-[#00241B]"} >Assignment Description:</p>
+              <textarea className={"pl-4"} value={descripiton} onChange={e => setDescription(e.target.value)} placeholder={"Enter description of assignment"} />
             </div>
-            <div className={"flex flex-row justify-start"}>
-              <button onClick={handleGenerateTasks}>generate</button>
+            <div className={"flex flex-row justify-start mt-4"}>
+              <button className="bg-[#65B891] ml-4 mb-4 rounded-lg text-[#00241B] p-2 text-center font-bold cursor-pointer" onClick={handleGenerateTasks}>Generate</button>
             </div>
           </>
         )
@@ -86,17 +86,17 @@ export default function PostAssignment() {
       {
         tasks.length !== 0 && (
           <>
-            <p>Select Skills per task</p>
+            <p className={"flex flex-row ml-4 font-bold my-3 text-l text-[#00241B]"} >Select Skills per task</p>
             <>
               {
                 tasks.map((task, index) => {
                   return (
                     <>
-                      <p>{task[0]}</p>
+                      <p className={"ml-4 font-bold text-[#00241B] mb-1"} >{task[0]}</p>
                       {
                         skills.map((skill) => {
                           return (
-                              <button className={"m-1"} onClick={() => handleAddSkill(index, skill)}>{skill}</button>
+                              <button className={"bg-[#65B891] ml-4 mb-4 rounded-lg text-[#00241B] p-2 text-center font-bold cursor-pointer"} onClick={() => handleAddSkill(index, skill)}>{skill}</button>
                             )
                         })
                       }
